08fda130942e69a24277beb4725dd8dd05a163bf
jrada <e12119059@student.tuwien.ac.at>
2025-11-04 19:46:05 +0100

    Backend Init

---
+++ LiDARC-backend/src/main/java/com/example/lidarcbackend/LiDarcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.lidarcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiDarcBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiDarcBackendApplication.class, args);
+    }
+
+}
