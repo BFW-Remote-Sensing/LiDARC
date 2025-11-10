5049e71d8e094a74c786eb57fbf8d5ba6cf48025
jrada <e12119059@student.tuwien.ac.at>
2025-11-10 10:37:17 +0100

    feat: setup rabbitmq connection, Refs: #28

--- LiDARC-worker/src/metadata_worker.py
+++ LiDARC-worker/src/metadata_worker.py
@@ -1,5 +1,35 @@
+import logging
+import time
+import pika
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(levelname)s | %(message)s')
+
+def process_message(ch, method, properties, body):
+    logging.info(f"Received message: {body.decode()}")
+
 def main():
-    print("Hello World! I give you Metadata!")
+    queue_name = "metadata_trigger"
+    logging.info(f"Connecting to RabbitMQ...")
+    while True:
+        try:
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(
+                    host="rabbitmq",
+                    port=5672,
+                    virtual_host="/",
+                    credentials=pika.PlainCredentials(username='admin', password='admin'),
+                )
+            )
+            channel = connection.channel()
+            channel.queue_declare(queue=queue_name, durable=True)
+            logging.info(f"Connected to RabbitMQ Listening on queue '{queue_name}'")
+            break
+        except Exception as e:
+            logging.warning(f"RabbitMQ Connection Error: {e}. Retrying in 5 seconds...")
+            time.sleep(5)
+
+    channel.basic_consume(queue=queue_name, on_message_callback=process_message, auto_ack=True)
+    channel.start_consuming()
 
 
 if __name__ == '__main__':
