11905291f60940f9f8d2e28d2d49f6460621ce16
jrada <e12119059@student.tuwien.ac.at>
2025-11-18 10:20:08 +0100

    feat: switched to file handler download in metadata worker instead of implementing own download function, Refs: #28

--- LiDARC-worker/src/metadata/metadata_worker.py
+++ LiDARC-worker/src/metadata/metadata_worker.py
@@ -11,35 +11,9 @@ import pika
 import requests
 from requests.adapters import HTTPAdapter, Retry
 from requests.exceptions import HTTPError
+import util.file_handler as file_handler
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(levelname)s | %(message)s')
-
-
-def download_file(url: str, dest_dir: str = ".", chunk_size: int = 10* 1024 ) -> str:
-    os.makedirs(dest_dir, exist_ok=True)
-    parsed = urlparse(url)
-    local_filename = os.path.join(dest_dir, os.path.basename(parsed.path))
-
-    session = requests.Session()
-    retries = Retry(
-        total=5,
-        backoff_factor=1,
-        status_forcelist=[500, 502, 503, 504],
-        allowed_methods=["GET"],
-    )
-    session.mount("https://", HTTPAdapter(max_retries=retries))
-    session.mount("http://", HTTPAdapter(max_retries=retries))
-    try:
-        with requests.get(url, stream=True) as r:
-            r.raise_for_status()
-            with open(local_filename, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=chunk_size):
-                    f.write(chunk)
-        return local_filename
-    except Exception as e:
-        if os.path.exists(local_filename):
-            os.remove(local_filename)
-        raise RuntimeError(f"Download failed for {url}: {e}") from e
 
 
 def parse_coordinate_system(header) -> str:
@@ -115,7 +89,15 @@ def process_req(ch, method, properties, body):
         las_file_url = req["url"]
         logging.info(f"Processing file from URL: {las_file_url}")
 
-        local_file = download_file(las_file_url)
+        local_file = ""
+        try:
+            local_file = file_handler.download_file(las_file_url)
+        except HTTPError as e:
+            logging.warning("Couldn't download file from: {}, error: {}".format(las_file_url, e))
+            return
+        if local_file == "":
+            logging.warning("File not downloaded, stopping processing the request!")
+            return
 
         metadata = extract_metadata(local_file)
         logging.info(f"Metadata extracted: {json.dumps(metadata)}")
