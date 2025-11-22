162b23bfa1e1f29893217a9d9b3d3d57c8b9dbab
jrada <e12119059@student.tuwien.ac.at>
2025-11-22 15:01:53 +0100

    feat: added small javadoc for processMetadata, Refs: #30

--- LiDARC-backend/src/main/java/com/example/lidarcbackend/service/files/IMetadataService.java
+++ LiDARC-backend/src/main/java/com/example/lidarcbackend/service/files/IMetadataService.java
@@ -3,5 +3,11 @@ package com.example.lidarcbackend.service.files;
 import java.util.Map;
 
 public interface IMetadataService {
-    void processMetadata(Map<String, Object> metadata);
+
+    /**
+     * Processes a metadata worker result message
+     *
+     * @param result the result message from the worker; can either be success message or error message
+     */
+    void processMetadata(Map<String, Object> result);
 }
