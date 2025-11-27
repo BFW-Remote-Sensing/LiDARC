c68c4aa64f603723afe7188e76c0995cf81f9380
Michael Mayr <e12122386@student.tuwien.ac.at>
2025-11-27 16:35:13 +0100

    ci: changed testcontainers and using services instead, test Refs: #31

--- LiDARC-worker/test/integration/conftest.py
+++ LiDARC-worker/test/integration/conftest.py
@@ -6,59 +6,109 @@ import pika
 import preprocess.preprocess_worker as preprocess
 from testcontainers.minio import MinioContainer
 from testcontainers.rabbitmq import RabbitMqContainer
+from minio import Minio
 
-minio = MinioContainer("registry.reset.inso-w.at/pub/docker/chainguard/minio:latest")
-rabbitmq = RabbitMqContainer("rabbitmq:3.12-management") #TODO: Update with registry in reset
+def running_in_ci_mode():
+    # Real GitLab CI
+    if os.getenv("GITLAB_CI") is not None:
+        return True
+    # gitlab-ci-local OR environments without docker.sock
+    if not os.path.exists("/var/run/docker.sock"):
+        return True
+    return False
 
 @pytest.fixture(scope="module", autouse=True)
 def minio_client(request, very_small_las_file):
-    minio.start()
+    running_in_ci = running_in_ci_mode()
+    if running_in_ci:
+        endpoint = os.getenv("MINIO_ENDPOINT")
+        access_key = os.getenv("MINIO_ACCESS_KEY")
+        secret_key = os.getenv("MINIO_SECRET_KEY")
 
-    def teardown():
-        minio.stop()
+        client = Minio(
+            endpoint,
+            access_key=access_key,
+            secret_key=secret_key,
+            secure=False
+        )
+        if not client.bucket_exists("basebucket"):
+            client.make_bucket("basebucket")
+        def upload_file(file_path, object_name="small.las"):
+            client.fput_object(bucket_name="basebucket",
+                               object_name=object_name,
+                               file_path=file_path)
 
-    request.addfinalizer(teardown)
-    os.environ["MINIO_ACCESS_KEY"] = minio.access_key
-    os.environ["MINIO_SECRET_KEY"] = minio.secret_key
-    os.environ["MINIO_ENDPOINT"] = f"{minio.get_container_host_ip()}:{minio.get_exposed_port(9000)}"
-    client = minio.get_client()
-    if not client.bucket_exists("basebucket"):
-        client.make_bucket("basebucket")
-    def upload_file(file_path, object_name="small.las"):
-        client.fput_object(bucket_name="basebucket",
-                       object_name=object_name,
-                       file_path=file_path)
+        yield client, upload_file
+        return
+    else:
+        minio = MinioContainer("registry.reset.inso-w.at/pub/docker/chainguard/minio:latest")
+        minio.start()
 
-        objects = list(client.list_objects("basebucket", recursive=True))
-        assert len(objects) > 0, "Expected atleast one object in bucket after upload"
-    yield client, upload_file
+        def teardown():
+            minio.stop()
+
+        request.addfinalizer(teardown)
+        os.environ["MINIO_ACCESS_KEY"] = minio.access_key
+        os.environ["MINIO_SECRET_KEY"] = minio.secret_key
+        os.environ["MINIO_ENDPOINT"] = f"{minio.get_container_host_ip()}:{minio.get_exposed_port(9000)}"
+        client = minio.get_client()
+        if not client.bucket_exists("basebucket"):
+            client.make_bucket("basebucket")
+        def upload_file(file_path, object_name="small.las"):
+            client.fput_object(bucket_name="basebucket",
+                           object_name=object_name,
+                           file_path=file_path)
+
+            objects = list(client.list_objects("basebucket", recursive=True))
+            assert len(objects) > 0, "Expected atleast one object in bucket after upload"
+        yield client, upload_file
 
 @pytest.fixture(scope="module", autouse=True)
 def rabbitmq_ch(request):
-    rabbitmq.start()
+    running_in_ci = running_in_ci_mode()
+    if running_in_ci:
+        username = os.getenv("RABBITMQ_USER")
+        password = os.getenv("RABBITMQ_PASSWORD")
+        host = os.getenv("RABBITMQ_HOST")
+        port = os.getenv("RABBITMQ_PORT")
+        vhost = os.getenv("RABBITMQ_VHOST")
+        exchange_name = os.getenv("EXCHANGE_NAME")
+        credentials = pika.PlainCredentials(username=username, password=password)
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=host, port=port, virtual_host=vhost, credentials=credentials))
+        ch = connection.channel()
+        yield ch
+        ch.queue_declare(queue="preprocessing.job", durable=True)
+        ch.exchange_declare(exchange=exchange_name, exchange_type="topic")
+        ch.queue_bind(queue="preprocessing.job", exchange=exchange_name, routing_key="preprocessing.job") #TODO: Fix the messaging in future or make it independent of real setup
+        ch.queue_declare(queue="preprocessing.result", durable=True)
+        ch.queue_bind(queue="preprocessing.result", exchange=exchange_name, routing_key="job.preprocessor.create")
+    else:
+        rabbitmq = RabbitMqContainer("rabbitmq:3.12-management")
+        rabbitmq.start()
 
-    def teardown():
-        rabbitmq.stop()
+        def teardown():
+            rabbitmq.stop()
 
-    request.addfinalizer(teardown)
-    rabbitmq.get_connection_params()
-    os.environ["RABBITMQ_USER"]= rabbitmq.username
-    os.environ["RABBITMQ_PASSWORD"] = rabbitmq.password
-    os.environ["RABBITMQ_HOST"] = rabbitmq.get_container_host_ip()
-    os.environ["RABBITMQ_PORT"] = str(rabbitmq.get_exposed_port(5672))
-    os.environ["RABBITMQ_VHOST"] =  rabbitmq.vhost
-    os.environ["EXCHANGE_NAME"] = "worker.job"
+        request.addfinalizer(teardown)
+        rabbitmq.get_connection_params()
+        os.environ["RABBITMQ_USER"]= rabbitmq.username
+        os.environ["RABBITMQ_PASSWORD"] = rabbitmq.password
+        os.environ["RABBITMQ_HOST"] = rabbitmq.get_container_host_ip()
+        os.environ["RABBITMQ_PORT"] = str(rabbitmq.get_exposed_port(5672))
+        os.environ["RABBITMQ_VHOST"] =  rabbitmq.vhost
+        os.environ["EXCHANGE_NAME"] = "worker.job"
 
-    credentials = pika.PlainCredentials(username=rabbitmq.username, password=rabbitmq.password)
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=rabbitmq.get_container_host_ip(), port=rabbitmq.get_exposed_port(5672), virtual_host=rabbitmq.vhost, credentials=credentials))
-    ch = connection.channel()
-    ch.queue_declare(queue="preprocessing.job", durable=True)
-    ch.exchange_declare(exchange="worker.job", exchange_type="topic")
-    ch.queue_bind(queue="preprocessing.job", exchange="worker.job", routing_key="preprocessing.job") #TODO: Fix the messaging in future or make it independent of real setup
-    ch.queue_declare(queue="preprocessing.result", durable=True)
-    ch.queue_bind(queue="preprocessing.result", exchange="worker.job", routing_key="job.preprocessor.create")
-    yield ch
+        credentials = pika.PlainCredentials(username=rabbitmq.username, password=rabbitmq.password)
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=rabbitmq.get_container_host_ip(), port=rabbitmq.get_exposed_port(5672), virtual_host=rabbitmq.vhost, credentials=credentials))
+        ch = connection.channel()
+        ch.queue_declare(queue="preprocessing.job", durable=True)
+        ch.exchange_declare(exchange="worker.job", exchange_type="topic")
+        ch.queue_bind(queue="preprocessing.job", exchange="worker.job", routing_key="preprocessing.job") #TODO: Fix the messaging in future or make it independent of real setup
+        ch.queue_declare(queue="preprocessing.result", durable=True)
+        ch.queue_bind(queue="preprocessing.result", exchange="worker.job", routing_key="job.preprocessor.create")
+        yield ch
 
 @pytest.fixture(scope="function", autouse=True)
 def setup_bucket():
